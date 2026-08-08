a392f18cdc9b74cac55fe5b484461d1d90d8b867
sistemaipda <sistemaipda1@gmail.com>
2026-08-08 19:01:22 -0300

    Atualizar o test.js

--- functions/test.js
+++ functions/test.js
@@ -1,5 +1,21 @@
 export default {
   async fetch(request) {
-    return new Response("Worker funcionando!");
+    const url = new URL(request.url);
+
+    if (url.pathname === "/functions/test") {
+      return new Response("Worker funcionando!");
+    }
+
+    if (url.pathname === "/functions/deauth") {
+      const ip = url.searchParams.get("ip");
+
+      if (!ip) {
+        return new Response("IP não informado", { status: 400 });
+      }
+
+      return new Response("Pedido de desconexão recebido para: " + ip);
+    }
+
+    return new Response("Not Found", { status: 404 });
   }
 };
